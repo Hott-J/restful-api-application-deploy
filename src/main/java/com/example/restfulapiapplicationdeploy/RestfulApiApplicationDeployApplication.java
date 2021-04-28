5b87a6159aaad81ed83680c3c5d89b94c4b8698e
Hott-J <jhj13062004@naver.com>
2021-04-28 20:07:32 +0900

    init

---
+++ src/main/java/com/example/restfulapiapplicationdeploy/RestfulApiApplicationDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulapiapplicationdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiApplicationDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiApplicationDeployApplication.class, args);
+	}
+
+}
