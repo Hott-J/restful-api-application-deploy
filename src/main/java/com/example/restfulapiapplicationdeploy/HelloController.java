221ea909424f80706a6732ad0b1818756770cfee
Hott-J <jhj13062004@naver.com>
2021-04-28 20:38:40 +0900

    add helloworldcontroller

---
+++ src/main/java/com/example/restfulapiapplicationdeploy/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.restfulapiapplicationdeploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "hello-world";
+    }
+}
