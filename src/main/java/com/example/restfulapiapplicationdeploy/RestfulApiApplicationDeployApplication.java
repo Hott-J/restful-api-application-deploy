b35292f3a94fd6ec9bc2fa4faead6d1d86446e43
Hott-J <jhj13062004@naver.com>
2021-04-28 20:15:55 +0900

    jar

--- src/main/java/com/example/restfulapiapplicationdeploy/RestfulApiApplicationDeployApplication.java
+++ src/main/java/com/example/restfulapiapplicationdeploy/RestfulApiApplicationDeployApplication.java
@@ -9,5 +9,4 @@ public class RestfulApiApplicationDeployApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApiApplicationDeployApplication.class, args);
 	}
-
 }
