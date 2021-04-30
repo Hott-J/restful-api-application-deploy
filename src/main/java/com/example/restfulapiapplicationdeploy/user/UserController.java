3904419d7f36e9cd3e4b87d800afb64fe6329cad
Hott-J <jhj13062004@naver.com>
2021-05-01 06:05:16 +0900

    HATEOAS

--- src/main/java/com/example/restfulapiapplicationdeploy/user/UserController.java
+++ src/main/java/com/example/restfulapiapplicationdeploy/user/UserController.java
@@ -1,12 +1,21 @@
 package com.example.restfulapiapplicationdeploy.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -18,19 +27,38 @@ public class UserController {
 
     // Read
     @GetMapping("/users")
-    public List<User> retrieveAllUsers(){
-        return service.findAll();
+    public MappingJacksonValue retrieveAllUsers(){
+
+        List<User> users = service.findAll();
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id","name","joinDate"); // 포함시킬 데이터
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo",filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(users);
+        mapping.setFilters(filters);
+        return mapping;
     }
 
     // GET /users/1 or /users/10 -> 10이란 id값도 서버측에서 문자열로 처리되어 전달됨
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){ // 위에서 보면 문자열로 처리되므로 int id로 해도되는 것
+    public MappingJacksonValue retrieveUser(@PathVariable int id){ // 위에서 보면 문자열로 처리되므로 int id로 해도되는 것
         User user = service.findOne(id);
         if(user==null){
             throw new UserNotFoundException(String.format("ID[%s] not found",id));
         }
 
-        return user;
+        // HATEOAS
+        EntityModel<User>model = new EntityModel<>(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkTo.withRel("all-users"));
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id","name","joinDate"); // 포함시킬 데이터
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo",filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(model);
+        mapping.setFilters(filters);
+        return mapping;
     }
 
     // Create
