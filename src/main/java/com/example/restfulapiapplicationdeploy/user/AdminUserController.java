2660ae4dee46b2151d9fc9ffba4c5a6126085b09
Hott-J <jhj13062004@naver.com>
2021-05-01 02:55:43 +0900

    version 관리

--- src/main/java/com/example/restfulapiapplicationdeploy/user/AdminUserController.java
+++ src/main/java/com/example/restfulapiapplicationdeploy/user/AdminUserController.java
@@ -3,6 +3,7 @@ package com.example.restfulapiapplicationdeploy.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -30,9 +31,12 @@ public class AdminUserController {
         return mapping;
     }
 
-    // GET admin/users/1 or admin/users/10 -> 10이란 id값도 서버측에서 문자열로 처리되어 전달됨
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable int id){ // 위에서 보면 문자열로 처리되므로 int id로 해도되는 것
+    // GET admin/v1/users/1 or admin/v1/users/10 -> 10이란 id값도 서버측에서 문자열로 처리되어 전달됨
+    @GetMapping("/v1/users/{id}")
+    //@GetMapping(value="/users/{id}/",params="version=1") // admin/users/1?version=1
+    //@GetMapping(value="/users/{id}",headers="X-API-VERSION=1") // 헤더에 입력
+    //@GetMapping("/v1/users/{id}",produces="application/vnd.company.appv1+json") // mime. 헤더에 입력. key=Accept. value에 produces 값넣는다
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id){ // 위에서 보면 문자열로 처리되므로 int id로 해도되는 것
         User user = service.findOne(id);
         if(user==null){
             throw new UserNotFoundException(String.format("ID[%s] not found",id));
@@ -44,6 +48,32 @@ public class AdminUserController {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo",filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/v2/users/{id}")
+    //@GetMapping(value="/users/{id}/",params="version=2") // admin/users/1?version=2
+    //@GetMapping(value="/users/{id}",headers="X-API-VERSION=2")
+    //@GetMapping("/v1/users/{id}",produces="application/vnd.company.appv2+json") // mime
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id){ // 위에서 보면 문자열로 처리되므로 int id로 해도되는 것
+        User user = service.findOne(id);
+        if(user==null){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+
+        // User -> User2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user,userV2); // id,name,joinDate,password,ssn
+        userV2.setGrade("VIP");
+
+        //필터링
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id","name","joinDate","grade"); // 포함시킬 데이터
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2",filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
         mapping.setFilters(filters);
 
         return mapping;
--- src/main/java/com/example/restfulapiapplicationdeploy/user/UserV2.java
+++ src/main/java/com/example/restfulapiapplicationdeploy/user/UserV2.java
@@ -11,17 +11,9 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 //@JsonIgnoreProperties(value={"password","ssn"}) // json 데이터 반환할때 해당 정보는 가림
-@JsonFilter("UserInfo") // 컨트롤러에서 필터링
-public class User {
-    private Integer id;
-
-    @Size(min=2, message="Name은 2글자 이상 입력해주세요.")
-    private String name;
-    @Past
-    private Date joinDate;
-
-    private String password;
-    private String ssn;
+@JsonFilter("UserInfoV2") // 컨트롤러에서 필터링
+@NoArgsConstructor
+public class UserV2 extends User{
+    private String grade;
 }
