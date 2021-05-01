72bb17ccef62036a87bceaaac6ddf91337feff3f
Hott-J <jhj13062004@naver.com>
2021-05-01 22:35:25 +0900

    jpa Post, Delete, Put 추가

--- src/main/java/com/example/restfulapiapplicationdeploy/user/UserJpaController.java
+++ src/main/java/com/example/restfulapiapplicationdeploy/user/UserJpaController.java
@@ -3,11 +3,12 @@ package com.example.restfulapiapplicationdeploy.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,5 +40,35 @@ public class UserJpaController {
         WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
         model.add(linkTo.withRel("all-users"));
         return model;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<Object> updateUser(@RequestBody User user, @PathVariable int id){
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if(!userOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        user.setId(id);
+
+        userRepository.save(user);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user values (1,sysdate(),'User1','pass1','ssn1');
-insert into user values (2,sysdate(),'User2','pass2','ssn2');
-insert into user values (3,sysdate(),'User3','pass3','ssn3');
+insert into user values (10001,sysdate(),'User1','pass1','ssn1');
+insert into user values (10002,sysdate(),'User2','pass2','ssn2');
+insert into user values (10003,sysdate(),'User3','pass3','ssn3');
