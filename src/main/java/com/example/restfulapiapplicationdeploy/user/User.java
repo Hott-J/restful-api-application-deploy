93120aca59260b58b07c533f12bd293e1d3109b0
Hott-J <jhj13062004@naver.com>
2021-05-01 19:47:28 +0900

    Spring Data JPA Entity 설정과 초기 데이터 생성

--- src/main/java/com/example/restfulapiapplicationdeploy/user/User.java
+++ src/main/java/com/example/restfulapiapplicationdeploy/user/User.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -17,7 +20,10 @@ import java.util.Date;
 //@JsonIgnoreProperties(value={"password","ssn"}) // json 데이터 반환할때 해당 정보는 가림
 @JsonFilter("UserInfo") // 컨트롤러에서 필터링
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체") // 스웨거 문서에 설명 추가
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message="Name은 2글자 이상 입력해주세요.")
@@ -25,7 +31,7 @@ public class User {
     private String name;
     @Past
     @ApiModelProperty(notes = "사용자 등록일을 입력해주세요.")
-    private Date joinDate;
+    private Date join_Date;
 
     @ApiModelProperty(notes = "사용자 비밀번호를 입력해주세요.")
     private String password;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values (1,sysdate(),'User1','pass1','ssn1');
+insert into user values (2,sysdate(),'User2','pass2','ssn2');
+insert into user values (3,sysdate(),'User3','pass3','ssn3');
